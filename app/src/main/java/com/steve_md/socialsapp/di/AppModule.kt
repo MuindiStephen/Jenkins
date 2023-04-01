c93d289a98910219de046d07cd045bdb83e51ce3
MuindiStephen <stephenmuindi241@gmail.com>
2023-04-01 17:50:56 +0300

    worked on hilt

--- app/src/main/java/com/steve_md/socialsapp/di/AppModule.kt
+++ app/src/main/java/com/steve_md/socialsapp/di/AppModule.kt
@@ -1,5 +1,59 @@
 package com.steve_md.socialsapp.di
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.steve_md.socialsapp.data.api.SocialsApiService
+import com.steve_md.socialsapp.screens.commons.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesBaseUrl() : String{
+        return BASE_URL
+    }
+
+    private val httpLoggingInterceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+
+
+    @Provides
+    @Singleton
+    fun providesGson() : Gson = GsonBuilder().setLenient().create()
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(
+        okHttpClient: OkHttpClient,
+        gson: Gson
+    ) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesSocialsApiService(retrofit: Retrofit) : SocialsApiService {
+        return retrofit.create(SocialsApiService::class.java)
+    }
 
 }
